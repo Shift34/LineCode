4c151e983c830a5db49db081513f6940a7122d30
zhoracementov <zoracementov92551@gmail.com>
2024-05-28 17:43:00 +0300

    decoding

---
+++ LineCode/EnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Line_Code__5_2_
+{
+    public static class EnumerableExtensions
+    {
+        public static string ToStringJoin<T>(this IEnumerable<T> enumerable)
+        {
+            return string.Join(string.Empty, enumerable);
+        }
+    }
+}
--- LineCode/Program.cs
+++ LineCode/Program.cs
@@ -1,91 +1,256 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Line_Code__5_2_
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static readonly int[,] G = new int[2, 5]
         {
-            int[,] G = new int[2, 5] { { 1, 0, 1, 0, 1 }, { 0, 1, 0, 1, 1 } };
-            Console.WriteLine("Введите кодовое слово:");
-            string input = Console.ReadLine();
-            List<int> Int_Input = new List<int>() { input[0] - '0', input[1] - '0' };
-            List<int> Encoded_Input = Multiply_Matrix(Int_Input, G);
-            Console.WriteLine("Закодированное сообщение:");
-            List<int> Encoded_Check = new List<int>(Encoded_Input);
-            foreach (int i in Encoded_Input)
+            { 1, 0, 1, 0, 1 },
+            { 0, 1, 0, 1, 1 }
+        };
+
+        private static readonly int[,] Transpant_H = new int[5, 3]
+        {
+            { 1, 0, 1 },
+            { 0, 1, 1 },
+            { 1, 0, 0 },
+            { 0, 1, 0 },
+            { 0, 0, 1 }
+        };
+
+        private static readonly Dictionary<string, int> Syndromes = new Dictionary<string, int>
+        {
+            { "000", 0 },
+            { "101", 1 },
+            { "011", 2 },
+            { "100", 3 },
+            { "010", 4 },
+            { "001", 5 },
+            { "110", -1 },
+            { "111", -1 }
+        };
+
+        static void Main()
+        {
+            for (int i = 0; i <= 1; i++)
             {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Введите индекс ошибки:");
-            int Index_Of_Mistake = int.Parse(Console.ReadLine()) - 1;
-            if (Index_Of_Mistake != -1)
-            {
-                Encoded_Input[Index_Of_Mistake] = (Encoded_Input[Index_Of_Mistake] + 1) % 2;
-            }
-            int[,] Transpant_H = new int[5, 3] { { 1, 0, 1 }, { 0, 1, 1 }, { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
-            List<int> Syndrome = Multiply_Matrix(Encoded_Input, Transpant_H);
-            Dictionary<string, int> Syndromes = new Dictionary<string, int>
-            {
-                { "000", 0 },
-                { "101", 1 },
-                { "011", 2 },
-                { "100", 3 },
-                { "010", 4 },
-                { "001", 5 },
-                { "110", -1 },
-                { "111", -1 }
-            };
-            string Str_Syn = string.Join("", Syndrome.ToArray());
-            string errorVector = "";
-            for (int i = 1; i < 6; i++)
-            {
-                if (i == Syndromes[Str_Syn])
+                for (int j = 0; j <= 1; j++)
                 {
-                    errorVector += "1";
+                    var list = new List<int> { i, j };
+                    var enc = Multiply_Matrix(list, G);
+
+                    for (int e = 0; e < 5; e++)
+                    {
+                        var encCopy = enc.ToList();
+                        encCopy[e] = (encCopy[e] + 1) % 2;
+
+                        Console.WriteLine($"Message: {i}{j}");
+                        Console.WriteLine($"Encoded: {enc.ToStringJoin()}");
+                        Console.WriteLine($"Error in {e + 1}");
+                        Console.WriteLine($"Message with error: {encCopy.ToStringJoin()}");
+                        var dec = Decoding(encCopy);
+                        Console.WriteLine($"Decoded: {dec.ToStringJoin()}");
+                        Console.WriteLine(new string('-', 30));
+                    }
                 }
-                else
-                {
-                    errorVector += "0";
-                }
             }
-            if (Syndromes[Str_Syn] > 0)
-            {
-                Encoded_Input[Syndromes[Str_Syn] - 1] = (Encoded_Input[Syndromes[Str_Syn] - 1] + 1) % 2;
-                Console.WriteLine($"Синдром: {Str_Syn}");
-                Console.WriteLine($"Вектор ошибки: {errorVector}");
-                Console.WriteLine($"Исправленная строка: {string.Join("", Encoded_Input.ToArray())}");
-            }
-            else if (Syndromes[Str_Syn] == 0)
-            {
-                Console.WriteLine("Ошибка не совершалась");
-            }
-            else
-            {
-                Console.WriteLine("Восстановить сообщение без ошибок невозможно");
-            }
+
             Console.ReadKey();
+            return;
+
+            //Console.WriteLine("Введите сообщение:");
+            //var input = Console.ReadLine();
+
+            //if (!Regex.IsMatch(input, @"^[01]+$"))
+            //{
+            //    Console.WriteLine("Ошибка ввода");
+            //    Console.ReadKey();
+            //    return;
+            //}
+
+            //var inputParsed = new List<int>
+            //{
+            //    input[0] - '0',
+            //    input[1] - '0'
+            //};
+
+            //var encoded = Multiply_Matrix(inputParsed, G);
+
+            //Console.WriteLine("Закодированное сообщение:");
+            //Console.WriteLine(encoded.ToStringJoin());
+
+            //Console.WriteLine("Введите индекс ошибки:");
+            //var errorIndex = int.Parse(Console.ReadLine()) - 1;
+            //if (errorIndex != -1)
+            //{
+            //    encoded[errorIndex] = (encoded[errorIndex] + 1) % 2;
+            //}
+            //else
+            //{
+            //    return;
+            //}
+
+            //var syndrome = Multiply_Matrix(encoded, Transpant_H).ToStringJoin();
+
+            //var errorVector = Enumerable.Repeat(0, 5).ToList();
+            //errorVector[Syndromes[syndrome] - 1] = 1;
+
+            ////if (Syndromes[syndrome] > 0)
+            ////{
+            ////    Console.WriteLine($"Ошибка в {Syndromes[syndrome]}-м бите");
+            ////    Console.WriteLine($"Синдром: {syndrome}");
+            ////    Console.WriteLine($"Вектор ошибки: {errorVector.ToStringJoin()}");
+            ////    Console.WriteLine($"Сообщение с ошибкой {encoded.ToStringJoin()}");
+            ////    //encoded = Sum_Vector(encoded, errorVector);
+            ////    Console.WriteLine($"Исправленная сообщение: {encoded.ToStringJoin()}");
+            ////}
+            ////else if (Syndromes[syndrome] == 0)
+            ////{
+            ////    Console.WriteLine("Ошибка не найдена");
+            ////}
+            ////else
+            ////{
+            ////    Console.WriteLine("Восстановить сообщение без ошибок невозможно");
+            ////}
+
+            //Console.WriteLine($"{encoded.ToStringJoin()}");
+            //var d = Decoding(encoded, Transpant_H);
+            //Console.WriteLine(d.ToStringJoin());
+
+            //Console.ReadKey();
         }
+
         public static List<int> Multiply_Matrix(List<int> str, int[,] Matrix)
         {
-            List<int> Answer = new List<int>();
-            //Умножение
-            for (int i = 0; i < Matrix.GetLength(1); i++)//номер столбца
+            var res = new List<int>();
+
+            for (int i = 0; i < Matrix.GetLength(1); i++) //номер столбца
             {
-                int sum = 0;
-                for (int j = 0; j < Matrix.GetLength(0); j++)//номер строчки 
+                var sum = 0;
+
+                for (int j = 0; j < Matrix.GetLength(0); j++) //номер строчки 
                 {
-                    sum += (Matrix[j, i] * (str[j]));
+                    sum += Matrix[j, i] * str[j];
                 }
-                Answer.Add(sum % 2);
+
+                res.Add(sum % 2);
             }
-            return Answer;
+
+            return res;
+        }
+
+        //public static List<int> Sum_Vector(List<int> vec1, List<int> vec2)
+        //{
+        //    var res = new List<int>(vec1.Count);
+
+        //    for (int i = 0; i < vec1.Count; i++)
+        //    {
+        //        res.Add((vec1[i] + vec2[i]) % 2);
+        //    }
+
+        //    return res;
+        //}
+
+        static List<int> Decoding(List<int> code)
+        {
+            //смежные классы:
+            var A0 = new int[4, 5]
+            {
+                { 0, 0, 0, 0, 0 },
+                { 1, 0, 1, 1, 1 },
+                { 0, 1, 0, 1, 1 },
+                { 1, 1, 1, 0, 0 }
+            };
+
+            var RelatedСlass = new List<int[,]>();
+
+            var vectors = new int[7, 5]
+            {
+                { 1, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 0 },
+                { 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 1, 0 },
+                { 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 1 },
+                { 1, 0, 0, 1, 0 },
+            };
+
+            for (int i = 0; i < 7; i++)
+            {
+                var Ai = new int[4, 5];
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int r = 0; r < 5; r++)
+                    {
+                        Ai[j, r] = (vectors[i, r] + A0[j, r]) % 2;
+                    }
+                }
+
+                RelatedСlass.Add(Ai);
+            }
+
+            //Console.WriteLine("Смежные классы не включая А0: ");
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)
+            //    {
+            //        for (int r = 0; r < 5; r++)
+            //        {
+            //            Console.Write($"{RelatedСlass[i][j, r]} ");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+
+            var syndrome = Multiply_Matrix(code, Transpant_H);
+
+            Console.WriteLine($"Синдром {syndrome.ToStringJoin()}");
+
+            if (syndrome.Count(x => x == 0) == syndrome.Count)
+            {
+                Console.WriteLine("Ошибок нет");
+                return code;
+            }
+
+            var index = Syndromes[syndrome.ToStringJoin()] - 1;
+
+            var NumVector = 0;
+            var minW = 6;
+
+            for (int j = 0; j < 4; j++)
+            {
+                var w = 0;
+                for (int r = 0; r < 5; r++)
+                {
+                    w += RelatedСlass[index][j, r];
+                }
+
+                if (minW > w)
+                {
+                    minW = w;
+                    NumVector = j;
+                }
+            }
+
+            var result = new List<int>();
+
+            for (int i = 0; i < code.Count; i++)
+            {
+                result.Add(code[i] - RelatedСlass[index][NumVector, i]);
+            }
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (result[i] < 0)
+                    result[i] = 2 + result[i];
+            }
+
+            return result;
         }
     }
 }
